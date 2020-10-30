5ceeb30c0010f85c707de05c6cf91254b11a5396
Ахмедова Фарзона Истамовна <65afi@cbr.ru>
2020-10-30 18:27:06 +0500

    Практические задачки 2

---
+++ src/main/java/BinarySearchTree/test/NodeTest.java
@@ -0,0 +1,15 @@
+package BinarySearchTree.test;
+
+import BinarySearchTree.Node;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NodeTest {
+
+    @Test
+    public void createNode() {
+        Node node = new Node(4);
+        assertThat(node.getValue()).isEqualTo(4);
+    }
+}
