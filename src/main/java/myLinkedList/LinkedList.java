40df9cfc3247474387b185b9055837e496c0b7dd
Ахмедова Фарзона Истамовна <65afi@cbr.ru>
2020-11-06 16:49:28 +0500

    Двусвязанный список

---
+++ src/main/java/myLinkedList/LinkedList.java
@@ -0,0 +1,13 @@
+package myLinkedList;
+
+public class LinkedList implements Methods {
+    @Override
+    public void put(Object value) {
+
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+}
---
+++ src/main/java/myLinkedList/Methods.java
@@ -0,0 +1,7 @@
+package myLinkedList;
+
+public interface Methods<T> {
+    void put(T value);
+
+    T get(int index);
+}
